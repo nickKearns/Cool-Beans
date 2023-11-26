8952882caeae4e80e86dcd59a097200f23f99a05
Nick Kearns <nickkearns133@gmail.com>
2023-11-26 14:28:50 -0500

    Initial Commit

---
+++ CoolBeans/CoolBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBeansApp.swift
+//  CoolBeans
+//
+//  Created by Nick Kearns on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct CoolBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
